e4238a2a7c0d684aa9818c5a46a80040856743c0
João Nascimento <Joaoncs21@gmail.com>
2024-08-14 22:33:13 -0300

    primeiro commit

---
+++ src/pacote1/Exercício.java
@@ -0,0 +1,41 @@
+package pacote1;
+
+public class Exercício {
+	public static void main(String args[]) {
+		//Integer
+		System.out.println("====int====");
+		int valor = 800000;
+		System.out.println(valor);
+		Integer valor1 = (Integer)valor;
+		System.out.println(valor1);
+		
+		//Short
+		System.out.println("====Short====");
+		short curto = 30;
+		System.out.println(curto);
+		Short curto1= (Short) curto;
+		System.out.println(curto1);
+		
+		//Long
+		System.out.println("====Long====");
+		long longo = 400000000l;
+		System.out.println(longo);
+		Long longo1 = (Long) longo;
+		System.out.println(longo1);
+		
+		//Float
+		System.out.println("====Float====");
+		float f = 70000;
+		System.out.println(f);
+		Float f1 = (Float)f;
+		System.out.println(f1);
+		
+		//Double
+		System.out.println("====Double====");
+		double db = 80000.0;
+		System.out.println(db);
+		Double db1 = (Double) db;
+		System.out.println(db1);
+		
+	}
+}
